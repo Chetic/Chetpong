ea683e1e384bc696b62b907fa2e6651e270ff99e
Chetic <Chetic@gmail.com>
2017-02-23 21:38:28 +0100

    Yey a triangle

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+extern GLuint LoadShaders(const char *vertex_file_path, const char *fragment_file_path);
+
 void killApplication(int exitCode) {
     glfwTerminate();
     exit(exitCode);
@@ -15,13 +17,19 @@ int main() {
         return 1;
     }
 
-    GLFWwindow* window = glfwCreateWindow(320, 240, "Chetpong", NULL, NULL);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(1024, 768, "Chetpong", NULL, NULL);
     if (window == NULL) {
         cerr << "Unable to create glfw window";
         killApplication(1);
     }
     glfwMakeContextCurrent(window);
 
+    glewExperimental = true;
     GLenum glewError = glewInit();
     if (glewError != GLEW_OK) {
         cerr << "Unable to init glew: " << glewGetErrorString(glewError);
@@ -29,11 +37,42 @@ int main() {
     }
 
     glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    GLuint vertexArrayId;
+    glGenVertexArrays(1, &vertexArrayId);
+    glBindVertexArray(vertexArrayId);
+
+    GLuint programID = LoadShaders("../vertex.glsl", "../fragment.glsl");
+
+    const GLfloat vertex_buffer_data[] = {
+            -1.0f, -1.0f, 0.0f,
+            1.0f, -1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+    };
+
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+
     while (glfwGetKey(window, GLFW_KEY_Q) != GLFW_PRESS &&
-            glfwWindowShouldClose(window) == false) {
+           glfwWindowShouldClose(window) == false) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(programID);
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *) 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
+
+    glDeleteBuffers(1, &vertexBuffer);
+    glDeleteVertexArrays(1, &vertexArrayId);
+    glDeleteProgram(programID);
 
     killApplication(0);
 }
